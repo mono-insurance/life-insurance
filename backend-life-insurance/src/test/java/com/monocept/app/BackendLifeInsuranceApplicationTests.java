3148fe41025c565ab90a024c4bb44d810fab7915
Agrah Jain <agrahjain02@gmail.com>
2024-09-02 01:35:06 +0530

    Initial Commit

---
+++ backend-life-insurance/src/test/java/com/monocept/app/BackendLifeInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monocept.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLifeInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
